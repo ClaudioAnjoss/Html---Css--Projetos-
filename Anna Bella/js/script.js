3f8669eb99c9f015d54ebc277933d6e0c655ad2d
Claudio Anjos <claudio.anjoss@outlook.com>
2022-06-20 21:06:15 -0300

    Modelo - Anna Bella (Concluido)

---
+++ Anna Bella/js/script.js
@@ -0,0 +1,16 @@
+// Menu responsivo
+
+let show = true;
+
+const menu = document.querySelector(".menu");
+const menuToggle = menu.querySelector(".menu__toggle");
+const main = document.querySelector("main");
+
+menuToggle.addEventListener("click", () => {
+
+    document.body.style.overflow = show ? "hidden" : "initial";
+    main.style.display = show ? "none" : "block";
+
+    menu.classList.toggle("on" , show);
+    show = !show;
+})
