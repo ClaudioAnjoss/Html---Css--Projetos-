40be1bd85ecda61dc892bb4838f472a7292f7ce7
Claudio Anjos <claudio.anjoss@outlook.com>
2022-06-23 21:45:17 -0300

    Noticias de cidades
    Pagina Inicial Finalizada

--- Site Noticias Cidade/js/menu_toggle.js
+++ Site Noticias Cidade/js/menu_toggle.js
@@ -4,12 +4,10 @@ let show = true;
 
 const menu = document.querySelector(".menu");
 const menuToggle = menu.querySelector(".menu__toggle");
-const main = document.querySelector("main");
 
 menuToggle.addEventListener("click", () => {
 
     document.body.style.overflow = show ? "hidden" : "initial";
-    main.style.display = show ? "none" : "block";
 
     menu.classList.toggle("active" , show);
     show = !show;
